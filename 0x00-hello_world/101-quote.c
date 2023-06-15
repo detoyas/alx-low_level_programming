86f19097f50d9b8c872d8909734ebd414204c8e9
Abdelali TAHA <abdeltahaali@gmail.com>
2023-06-15 10:45:25 -0700

    Adding The last advanced task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: program that prints to the standard error
+ *
+ * Return: Always 1 (Error)
+ */
+
+int main(void)
+{
+	char quot[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(STDERR_FILENO, quot, 58);
+	return (1);
+}
