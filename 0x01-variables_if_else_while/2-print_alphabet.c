dbba1fcbdc3f9fb346f1c65eb62308b75a7e1d96
Abdelali TAHA <abdeltahaali@gmail.com>
2023-06-16 09:50:00 -0700

    Adding Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Program that prints the alphabet in lower case
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+}
