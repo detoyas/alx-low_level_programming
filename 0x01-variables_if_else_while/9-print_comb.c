525fb0b368f1b45604099ba4d6ed56bf6d2d4ead
Abdelali TAHA <abdeltahaali@gmail.com>
2023-06-16 13:05:57 -0700

    Adding task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Program that prints possible single digits numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		putchar('0' + i);
+
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	i++;
+	}
+	return (0);
+}
