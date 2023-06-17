444417e38276cf498c44a1ee4c5dcc9cfcbda124
Abdelali TAHA <abdeltahaali@gmail.com>
2023-06-16 18:56:32 -0700

    Fixing Last advanced task

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -19,14 +19,14 @@ int main(void)
 		{
 			if (digit2 != digit1)
 			{
-				putchar((digit1 / 10) + '0');
-				putchar((digit1 % 10) + '0');
+				putchar((digit1 / 10) + 48);
+				putchar((digit1 % 10) + 48);
 				putchar(' ');
-				putchar((digit2 / 10) + '0');
-				putchar((digit2 % 10) + '0');
+				putchar((digit2 / 10) + 48);
+				putchar((digit2 % 10) + 48);
 			}
 
-			if (digit1 != 99 || digit2 != 99)
+			if (digit1 != 98 || digit2 != 99)
 			{
 				putchar(',');
 				putchar(' ');
