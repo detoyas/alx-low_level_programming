fa3310e0004a9a245325f1e0eb8dc0b61d33bb99
Abdelali TAHA <abdeltahaali@gmail.com>
2023-06-20 13:06:32 -0700

    Adding task7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit of num
+ *
+ * @num: input number
+ *
+ * Return: lastdigit
+ */
+
+int print_last_digit(int num)
+{
+	int lastdigit = num % 10;
+
+	if (lastdigit < 0)
+	{
+		lastdigit = -1 * last_digit;
+	}
+
+	_putchar(lastdigit + '0');
+
+	return (lastdigit);
+}
