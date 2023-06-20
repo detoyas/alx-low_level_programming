bd6fb41d0ddc01414e23dc07f3b124cf8d6d7f89
Abdelali TAHA <abdeltahaali@gmail.com>
2023-06-20 09:51:14 -0700

    Adding task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - printing the alphabet using _putchar
+ */
+
+void print_alphabet(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
