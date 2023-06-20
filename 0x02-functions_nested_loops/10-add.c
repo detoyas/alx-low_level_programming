ff923be517e41ee3a428b9de88a4ede483c43cc8
Abdelali TAHA <abdeltahaali@gmail.com>
2023-06-20 14:29:57 -0700

    Adding task10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * add - addition function
+ * @x: first integer
+ * @y: second integer
+ *
+ * Return: sum of integers
+ *
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
