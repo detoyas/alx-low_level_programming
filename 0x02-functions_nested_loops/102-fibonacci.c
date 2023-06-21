98123a8cf537901c1eb94c89896a7872793c7af0
Abdelali TAHA <abdeltahaali@gmail.com>
2023-06-20 17:00:37 -0700

    Adding advanced task

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Descriptipn: print Fibonacci numbers 
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int count;
+	long int fib1 = 0;
+	long int fib2 = 1;
+	long int sum;
+
+	for (count =0; count < 50; count++)
+	{
+		sum = fib1 + fib2;
+		printf("%ld", sum);
+
+		fib1 = fib2;
+		fib2 = sum
+
+		if (count < 50)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
