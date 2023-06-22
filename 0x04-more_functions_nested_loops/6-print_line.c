0c5dd5125b64824fd37d1c348c33a275be8baa07
Abdelali TAHA <abdeltahaali@gmail.com>
2023-06-22 12:10:41 -0700

    Adding Task6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,16 +9,10 @@ void print_line(int n)
 {
 	int i;
 
-	if (n <= 0)
+	if (n > 0)
 	{
-		_putchar('\n');
-		break;
-	}
-
-	for (i = 0; i <= n; i++)
-	{
-		_putchar('_');
+		for (i = 0; i <= n; i++)
+			_putchar('_');
 	}
 	_putchar('\n');
-
 }
