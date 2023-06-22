e48bdc9239087a5e6051c4803ff4b7fdd5bb8c45
Abdelali TAHA <abdeltahaali@gmail.com>
2023-06-22 11:41:42 -0700

    Adding Task5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,23 +1,22 @@
 #include "main.h"
 
 /**
- * more_numbers - print using _putchar from 0 to 14
- */
+*more_numbers - print 10 times two digits numbers
+*/
 
 void more_numbers(void)
 {
-	int i, j;
+	int i;
+	int j;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
 			if (j > 9)
-			{
 				_putchar((j / 10) + '0');
-			}
 			_putchar((j % 10) + '0');
 		}
-		_putchar('\n');
+	_putchar('\n');
 	}
-{
+}
