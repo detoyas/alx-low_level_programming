729a0a8c90a36779ce61dff4b51f0f3dab778b25
Abdelali TAHA <abdeltahaali@gmail.com>
2023-06-26 13:15:21 -0700

    Adding Task4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: string to print
+ */
+void print_rev(char *s)
+{
+	int i, j, length;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	length = i;
+
+	for (j = length - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+
+	_putchar('\n');
+}
