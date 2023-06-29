28e55665b7553bc2006139acf52e6a82600bb88b
Abdelali TAHA <abdeltahaali@gmail.com>
2023-06-29 13:25:30 -0700

    Adding Task0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,22 +9,24 @@
  * Return: pointer to string dest
  */
 
-char *_strncat(char *dest, char *src, int n)
+char *_strcat(char *dest, char *src)
 {
-	int i, j;
+	int i = 0;
+	int j = 0;
 
-	i = 0;
-	j = 0;
-
-	while (dest[i])
-		i++;
-	
-	
-	while (src[j])
+	while (dest[i] != '\0')
 	{
-		dest[i++] = src[j];
-		j++;
+		i++;
 	}
+
+	while (src[j] != '\0')
+	{
+		dest[i + j] = src[j];
+		j;
+
+	}
+	dest[i + j] = '\0';
 
 	return (dest);
 }
+
