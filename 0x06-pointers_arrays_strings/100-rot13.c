e690df8ffeb8125bae6c5bc0480062104c5db5b0
Abdelali TAHA <abdeltahaali@gmail.com>
2023-06-29 14:55:30 -0700

    Adding first advanced task

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rot13 - encrypts string using rot13
+ * @s: string to encrypt
+ * Return: 
+ */
+
+char *rot13(char *s)
+{
+	char p1[52] = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
+	char p2[52] = "nNoOpPqQrRsStTuUvVwWxXyYzZaAbBcCdDeEfFgGhHiIjJkKlLmM";
+
+	int i;
+	int j = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+
+		for (j = 0; part1[j] != '\0'; j++)
+		{
+			if (s[i] == p1[j])
+			{
+				s[i] = p2[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
