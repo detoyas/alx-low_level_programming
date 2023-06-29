dfa7a4e309f77b743ca74c2173b4e8fd5e9b6725
Abdelali TAHA <abdeltahaali@gmail.com>
2023-06-29 13:13:57 -0700

    Adding Task0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,15 +14,14 @@ char *_strncat(char *dest, char *src, int n)
 	int i, j;
 
 	i = 0;
-	j = 0;
-
+	
 	while (dest[i] != '\0')
 		i++;
-
-	while (src[j] != '\0' && j < n)
+	
+	j = 0;
+	while (src[j] != '\0' && )
 	{
-		dest[i] = src[j];
-		i++;
+		dest[i++] = src[j];
 		j++;
 	}
 
