8ac9abbedfb65fad978b294bf26bb38f60660165
Abdelali TAHA <abdeltahaali@gmail.com>
2023-07-03 08:06:00 -0700

    Adding Task0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_memset - function that fills memory with a constant byte
+ * @s: pointer to the memory area
+ * @b: constant byte to assign for each byte
+ * @n: number of bytes
+ * Return: pointer s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	while (n > 0)
+	{
+		s = b;
+		s++;
+		n--;
+	}
+	return (s);
+}
