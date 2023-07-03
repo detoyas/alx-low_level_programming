81f869f5b78de57cfacb8e2493b7c91f4147b39e
Abdelali TAHA <abdeltahaali@gmail.com>
2023-07-03 11:39:20 -0700

    Adding task3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _strspn - Function that gets the length of a prefix substring
+ * @s: String to search
+ * @accept: acceptable bytes string can contain
+ *
+ * Return: Length
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j, k, a;
+
+	i = j = 0;
+	while (s[i] != 0)
+	{
+		k = 0;
+		a = 0;
+		while (accept[k] != 0)
+		{
+			if (s[i] != accept[k])
+			{
+				k++;
+			}
+			else if (s[i] == accept[k])
+			{
+				a++;
+				j++;
+				k++;
+			}
+		}
+		if (a == 0)
+			return (j);
+		i++;
+	}
+	return (j);
+}
