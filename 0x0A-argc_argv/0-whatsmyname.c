2b0e6658fa6b172e897a6de6850322e36ac43638
Abdelali TAHA <abdeltahaali@gmail.com>
2023-07-07 10:18:44 -0700

    Adding Task0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - function that prints program name
+ * @argc: number of command-line arguments passed by the user
+ * @argv: an array of pointers to the strings
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
