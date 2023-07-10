b787e7277765db4b38491d80c60d04b6e2225030
Abdelali TAHA <abdeltahaali@gmail.com>
2023-07-10 04:44:02 -0700

    Adding Task0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,9 +13,9 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return(NULL);
 	
-	int *a;
+	char *a;
 
-	for (i = 0; i < size; i++)
+	for (int i = 0; i < size; i++)
 		a[i] = c;
 	return (a);
 }
