cad1ae1c35e36dcda0110a6095e22ec9a49099f4
Abdelali TAHA <abdeltahaali@gmail.com>
2023-07-10 04:57:37 -0700

    Adding Task0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,12 +10,12 @@
 
 char *create_array(unsigned int size, char c)
 {
+	char *a;
+        unsigned int i;
+	
 	if (size == 0)
 		return(NULL);
 	
-	char *a;
-	unsigned int i;
-
 	a = malloc(sizeof(char) * size);
 	if (a == NULL)
 		return (NULL);
