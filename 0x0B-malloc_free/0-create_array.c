303c05576d5571fb361cb4671a511e05dccdf742
Abdelali TAHA <abdeltahaali@gmail.com>
2023-07-10 05:01:06 -0700

    Adding Task0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,19 +3,19 @@
 /**
  * create_array - function that creates an array of chars
  * @size: size of the memory to print
- * @c: characters to print 
+ * @c: characters to print
  *
  * Return: pointer to the array
- * */
+ */
 
 char *create_array(unsigned int size, char c)
 {
 	char *a;
-        unsigned int i;
-	
+	unsigned int i;
+
 	if (size == 0)
-		return(NULL);
-	
+		return (NULL);
+
 	a = malloc(sizeof(char) * size);
 	if (a == NULL)
 		return (NULL);
