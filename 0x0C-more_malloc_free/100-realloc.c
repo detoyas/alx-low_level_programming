7a0439c25e4187027f8ba1ae98d29b2819deff6f
Abdelali TAHA <abdeltahaali@gmail.com>
2023-07-13 13:51:17 -0700

    Adding advanced task

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * *_realloc - reallocates a memory block 
+ * @ptr: pointer to old memory location
+ * @old_size: unsigned int old memory size
+ * @new_size: unsigned int new memory size
+ * Return: pointer to array
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *s;
+
+	if (new_size > old_size)
+	{
+		s = malloc(new_size);
+		free(ptr);
+		return (s);
+	}
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
+	if (ptr == NULL)
+	{
+		s = malloc(new_size);
+		free(ptr);
+		return (s);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	return (ptr);
+}
