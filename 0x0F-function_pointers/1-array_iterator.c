69338380048e2af3e54e71d4737313295933a7cc
Abdelali TAHA <abdeltahaali@gmail.com>
2023-07-19 09:03:59 -0700

    Adding Task1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator -  executes a function given as a parameter
+ * @array: integer array
+ * @size: array size
+ * @action: function pointer
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int *end = array + size - 1;
+
+	if (array && size && action)
+		while (array <= end)
+			action(*array++);
+}
