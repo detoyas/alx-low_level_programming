fbb35e3645cbf3b7b5b5e75319ed90a1fd19106b
Abdelali TAHA <abdeltahaali@gmail.com>
2023-07-20 16:37:12 -0700

    Updating header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,7 +3,20 @@
 
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
 
+/**
+ * struct data - Struct data type
+ *
+ * @data: format data
+ * @f: function associated
+ */
+
+typedef struct data
+{
+	char *data;
+	void (*f)(char *, va_list);
+} data_t;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
