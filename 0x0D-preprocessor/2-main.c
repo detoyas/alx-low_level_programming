e37e29e4698fcc77f8f971b6e9fb6fbf6f51ca76
Abdelali TAHA <abdeltahaali@gmail.com>
2023-09-11 12:38:16 -0700

    Adding Task2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - the name of the file it was compiled from
+ * Return: always 0 on success
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
